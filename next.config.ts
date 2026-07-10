c455b35894dd118ac479c8e18e7da85e898995ec
hardik <singhhardiksingh3210@gmail.com>
2026-07-10 22:50:39 +0530

    Fix Next config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../../../"),
-  experimental: {
-    devtoolSegmentExplorer: false,
-  },
   images: {
     remotePatterns: [
       {
